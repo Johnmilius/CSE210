71dd1f92db997dff16abbde1df1746776406c183
John Milius <mil24086@byui.edu>
2025-04-28 09:34:31 -0600

    C# prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("");
+
+        //Get magic Number
+        Console.WriteLine("What is the magic number? ");
+        string strMagicNumber = Console.ReadLine();
+        int magicNumber = int.Parse(strMagicNumber);
+
+        //Get loop for guess the end game when guessed right
+        int userGuess = magicNumber + 1; // The plus one makes it not end the loop without guessing. 
+        string strUserGuess = "";
+
+        while (userGuess != magicNumber)
+        {
+
+            Console.WriteLine("What is your guess? ");
+            strUserGuess = Console.ReadLine();
+            userGuess = int.Parse(strUserGuess);
+
+            //Check if guess equals the magic number
+            if (magicNumber == userGuess)
+            {
+                Console.WriteLine($"You Guessed the Maigc number of {magicNumber}");
+            }
+            else
+            {
+                if (magicNumber < userGuess) { Console.WriteLine("Lower"); }
+                else { Console.WriteLine("Higher"); }
+            }
+
+        }
+
     }
 }

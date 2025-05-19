48822f3a60de9d97fd91d012d5a817d9e9138193
John Milius <mil24086@byui.edu>
2025-05-19 10:01:10 -0600

    program in Learning 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Fraction f2 = new Fraction(3, 4);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+
     }
 }
